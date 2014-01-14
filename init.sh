4d29bf99964ce860f4dea8b31f765d9d37006eb8
Erik Fercak <erik.fercak@gmail.com>
2014-01-14 09:58:02 +0100

    Fix errors in init script.

--- init.sh
+++ init.sh
@@ -2,12 +2,12 @@
 
 pushd `pwd`
 git clone https://github.com/vim-php/phpctags ~/.vim/bin/phpctags
-cd ~/.vim/phpctags && make
+cd ~/.vim/bin/phpctags && make
 popd
 
 mkdir ~/.vim/spell
-wget http://ftp.vim.org/vim/runtime/spell/sk.utf-8.spl ~/.vim/spell/sk.utf-8.spl
-wget http://ftp.vim.org/vim/runtime/spell/cs.utf-8.spl ~/.vim/spell/cs.utf-8.spl
+wget http://ftp.vim.org/vim/runtime/spell/sk.utf-8.spl -O ~/.vim/spell/sk.utf-8.spl
+wget http://ftp.vim.org/vim/runtime/spell/cs.utf-8.spl -O ~/.vim/spell/cs.utf-8.spl
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim --noplugin -u ~/.vim/vundles.vim -N "+set hidden" "+syntax on" +BundleClean! +BundleInstall +qall
