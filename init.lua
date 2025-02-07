fc3602136496a2b4ac665450725e56aa62cf70a9
Erik Fercak <erik.fercak@gmail.com>
2025-02-07 16:34:22 +1300

    Use blink.nvim plus minor things

--- init.lua
+++ init.lua
@@ -142,13 +142,23 @@ require("lazy").setup({
 			config = function()
 				local actions = require("telescope.actions")
 				local layout_actions = require("telescope.actions.layout")
+
 				require("telescope").setup({
 					defaults = {
 						preview = { hide_on_startup = true },
 						mappings = {
 							i = {
 								["<esc>"] = actions.close,
-								-- ["<C-v>"] = layout_actions.toggle_preview,
+								["<C-v>"] = layout_actions.toggle_preview,
+							},
+						},
+					},
+					pickers = {
+						buffers = {
+							mappings = {
+								i = {
+									["<c-d>"] = actions.delete_buffer,
+								},
 							},
 						},
 					},
@@ -234,19 +244,60 @@ require("lazy").setup({
 		{
 			"williamboman/mason-lspconfig.nvim",
 			opts = {
-				ensure_installed = { "lexical", "lua_ls", "gopls", "rust_analyzer", "zls" },
+				ensure_installed = { "lexical", "lua_ls", "gopls", "ruby_lsp", "rust_analyzer", "zls" },
 			},
+		},
+		{
+			"saghen/blink.cmp",
+			-- optional: provides snippets for the snippet source
+			dependencies = "rafamadriz/friendly-snippets",
+
+			-- use a release tag to download pre-built binaries
+			version = "v0.*",
+			-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+			-- build = 'cargo build --release',
+			-- If you use nix, you can build from source using latest nightly rust with:
+			-- build = 'nix run .#build-plugin',
+
+			---@module 'blink.cmp'
+			---@type blink.cmp.Config
+			opts = {
+				-- 'default' for mappings similar to built-in completion
+				-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+				-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+				-- see the "default configuration" section below for full documentation on how to define
+				-- your own keymap.
+				keymap = { preset = "default" },
+
+				appearance = {
+					-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+					-- Useful for when your theme doesn't support blink.cmp
+					-- will be removed in a future release
+					use_nvim_cmp_as_default = true,
+					-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+					-- Adjusts spacing to ensure icons are aligned
+					nerd_font_variant = "mono",
+				},
+
+				-- default list of enabled providers defined so that you can extend it
+				-- elsewhere in your config, without redefining it, via `opts_extend`
+				sources = {
+					default = { "lsp", "path", "snippets", "buffer" },
+					-- optionally disable cmdline completions
+					-- cmdline = {},
+				},
+
+				-- experimental signature help support
+				signature = { enabled = true },
+			},
+			-- allows extending the providers array elsewhere in your config
+			-- without having to redefine it
+			opts_extend = { "sources.default" },
 		},
 		{
 			"neovim/nvim-lspconfig",
 			dependencies = {
-				"hrsh7th/cmp-nvim-lsp",
-				"hrsh7th/nvim-cmp",
-				"hrsh7th/cmp-buffer",
-				"hrsh7th/cmp-path",
-				"hrsh7th/cmp-cmdline",
-				"hrsh7th/cmp-nvim-lua",
-				"L3MON4D3/LuaSnip",
+				"saghen/blink.cmp",
 				-- "folke/neodev.nvim",
 				"j-hui/fidget.nvim",
 			},
@@ -254,33 +305,38 @@ require("lazy").setup({
 				-- require("neodev").setup({})
 				require("fidget").setup({})
 
+				local default_capabilities = require("blink.cmp").get_lsp_capabilities()
+				--local default_capabilities = vim.lsp.protocol.make_client_capabilities()
 				local lspconfig = require("lspconfig")
 
-				local default_capabilities = vim.lsp.protocol.make_client_capabilities()
-				default_capabilities =
-					vim.tbl_deep_extend("force", default_capabilities, require("cmp_nvim_lsp").default_capabilities())
+				-- default_capabilities =
+				-- 	vim.tbl_deep_extend("force", default_capabilities, require("cmp_nvim_lsp").default_capabilities())
 				-- local default_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-				-- lspconfig.elixirls.setup({
-				-- 	cmd = { "elixir-ls" },
+				lspconfig.elixirls.setup({
+					cmd = { "/Users/erik/.local/share/nvim/mason/bin/elixir-ls" },
+					capabilities = default_capabilities,
+					-- settings = {
+					-- 	elixirLS = {
+					-- 		dialyzerEnabled = false,
+					-- 		fetchDeps = false,
+					-- 	},
+					-- },
+				})
+
+				-- lspconfig.lexical.setup({
+				-- 	filetypes = { "elixir", "eelixir", "heex" },
+				-- 	cmd = { "/Users/erik/.local/share/nvim/mason/bin/lexical", "server" },
+				-- 	-- cmd = { "/Users/erik/.local/share/nvim/mason/packages/lexical/libexec/lexical/bin/start_lexical.sh" },
+				-- 	root_dir = require("lspconfig.util").root_pattern({ "mix.exs", ".git" }),
 				-- 	capabilities = default_capabilities,
-				-- 	settings = {
-				-- 		elixirLS = {
-				-- 			dialyzerEnabled = false,
-				-- 			fetchDeps = false,
-				-- 		},
-				-- 	},
 				-- })
 
-				lspconfig.lexical.setup({
-					filetypes = { "elixir", "eelixir", "heex" },
-					cmd = { "/Users/erik/.local/share/nvim/mason/bin/lexical", "server" },
-					-- cmd = { "/Users/erik/.local/share/nvim/mason/packages/lexical/libexec/lexical/bin/start_lexical.sh" },
-					root_dir = require("lspconfig.util").root_pattern({ "mix.exs", ".git" }),
+				lspconfig.zls.setup({
 					capabilities = default_capabilities,
 				})
 
-				lspconfig.zls.setup({
+				lspconfig.ruby_lsp.setup({
 					capabilities = default_capabilities,
 				})
 
@@ -379,74 +435,6 @@ require("lazy").setup({
 						end
 					end,
 				})
-
-				local cmp = require("cmp")
-				-- local luasnip = require("luasnip")
-				-- luasnip.config.setup({})
-
-				cmp.setup({
-					sources = cmp.config.sources(
-						{ { name = "nvim_lsp" } },
-						{ { name = "buffer", option = { keyword_length = 4 } } },
-						{ { name = "path" } }
-					),
-					completion = { completeopt = "menu,menuone,noinsert" },
-					mapping = cmp.mapping.preset.insert({
-						-- Select the next item
-						["<C-n>"] = cmp.mapping.select_next_item(),
-						-- Select the previous item
-						["<C-p>"] = cmp.mapping.select_prev_item(),
-
-						-- Accept currently selected item. Set `select` to `false`
-						-- to only confirm explicitly selected items.
-						["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-						-- Ctrl + space triggers completion menu
-						["<C-Space>"] = cmp.mapping.complete(),
-
-						["<C-e>"] = cmp.mapping.abort(),
-						["<C-b>"] = cmp.mapping.scroll_docs(-4),
-						["<C-f>"] = cmp.mapping.scroll_docs(4),
-					}),
-
-					snippet = {
-						expand = function(args)
-							require("luasnip").lsp_expand(args.body)
-						end,
-					},
-
-					experimental = {
-						native_menu = false,
-						ghost_text = true,
-					},
-				})
-
-				-- Set configuration for specific filetype.
-				cmp.setup.filetype("lua", {
-					sources = cmp.config.sources({
-						{ name = "nvim_lua" },
-					}, {
-						{ name = "buffer", option = { keyword_length = 4 } },
-					}),
-				})
-
-				-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-				cmp.setup.cmdline({ "/", "?" }, {
-					mapping = cmp.mapping.preset.cmdline(),
-					sources = {
-						{ name = "buffer" },
-					},
-				})
-
-				-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-				cmp.setup.cmdline(":", {
-					mapping = cmp.mapping.preset.cmdline(),
-					sources = cmp.config.sources({
-						{ name = "path" },
-					}, {
-						{ name = "cmdline" },
-					}),
-				})
 			end,
 		},
 		{
@@ -523,7 +511,7 @@ require("lazy").setup({
 						go = { "gofmt" },
 						lua = { "stylua" },
 						opentofu = { "tofu_fmt" },
-						ruby = { "rubocop" },
+						ruby = { "standardrb" },
 						rust = { "rustfmt" },
 						terraform = { "tofu_fmt" },
 						-- Conform can also run multiple formatters sequentially
